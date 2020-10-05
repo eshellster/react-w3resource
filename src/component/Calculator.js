ee6f798ac4de77089107d5a19793c81e7040ac72
eshell <eshellster@gmail.com>
2020-10-05 14:50:55 +0900

    온도계 모듈화

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -7,7 +7,12 @@ function BoilingVerdict(props) {
   return "The water would not boil.";
 }
 
-class Calculator extends React.Component {
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
@@ -20,12 +25,24 @@ class Calculator extends React.Component {
 
   render() {
     const temperature = this.state.temperature;
+    const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
         <input value={temperature} onChange={this.handleChange} />
         <BoilingVerdict celsius={parseFloat(temperature)} />
       </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale={"c"} />
+        <TemperatureInput scale={"f"} />
+      </div>
     );
   }
 }

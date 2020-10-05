816cece3b8532d55a586b8c25daa3112777bddb0
eshell <eshellster@gmail.com>
2020-10-05 20:24:50 +0900

    다중 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
-import EssayForm from "./component/form/EssayForm";
-import FlavorForm from "./component/form/FlavorForm";
-import NameForm from "./component/form/NameForm";
+import Reservation from "./component/form/Reservation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NameForm />
-        <EssayForm />
-        <FlavorForm />
+        <Reservation />
       </header>
     </div>
   );

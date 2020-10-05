0fec479aa40552863dfdbed32c992a2656078a49
eshell <eshellster@gmail.com>
2020-10-05 16:52:40 +0900

    상태를 부모요소로 올려 관리하기

--- src/component/TemperatureInput.js
+++ src/component/TemperatureInput.js
@@ -1,12 +1,5 @@
 import React from "react";
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return "The water would boil.";
-  }
-  return "The water would not boil.";
-}
-
 const scaleNames = {
   c: "Celsius",
   f: "Fahrenheit",
@@ -16,21 +9,20 @@ class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
+    // this.state = { temperature: "" };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
         <legend>Enter temperature in {scaleNames[scale]}:</legend>
         <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
       </fieldset>
     );
   }

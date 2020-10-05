410fefe448494cd44d54aaafc9461b25bba965e6
eshell <eshellster@gmail.com>
2020-10-05 14:55:36 +0900

    온도 인풋 분리

---
+++ src/component/TemperatureInput.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return "The water would boil.";
+  }
+  return "The water would not boil.";
+}
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
+  }
+
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;

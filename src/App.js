a9ee7816cd25baf8c4c8a20d6ac361d6852224ed
eshell <eshellster@gmail.com>
2020-10-09 18:32:12 +0900

    마우스 트랙커

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
 import React from "react";
 import "./App.css";
-import Hoc from "./component/hoc/HOC";
-import StockList from "./component/hoc/StockList";
-import UserList from "./component/hoc/UserList";
-
-const StocksData = [
-  {
-    id: 1,
-    name: "TCS",
-  },
-  {
-    id: 2,
-    name: "Infosys",
-  },
-  {
-    id: 3,
-    name: "Reliance",
-  },
-];
-const UsersData = [
-  {
-    id: 1,
-    name: "Krunal",
-  },
-  {
-    id: 2,
-    name: "Ankit",
-  },
-  {
-    id: 3,
-    name: "Rushabh",
-  },
-];
-
-const Stocks = Hoc(StockList, StocksData);
-
-const Users = Hoc(UserList, UsersData);
+import { MouseTracker } from "./component/propsRender/MouseTracker";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Stocks title="Stocks" />
-        <Users title="User" />
+        <MouseTracker />
       </header>
     </div>
   );
---
+++ src/component/propsRender/MouseTracker.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export const MouseTracker = () => {
+  const [state, setState] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setState({ x: event.clientX, y: event.clientY });
+  };
+  return (
+    <div style={{ height: "100%" }} onMouseMove={handleMouseMove}>
+      <h1>Move the mouse around!</h1>
+      <p>
+        The current mouse position is ({state.x}, {state.y})
+      </p>
+    </div>
+  );
+};
